9a7fb4bf2c8943d06f8393e9c7ff4bee3aff6bda
Valdemar Santos <valdemarnoivo@hotmail.com>
2025-07-10 21:43:51 +0100

    Add manifest + icons, update public alert page and static config

--- config/settings.py
+++ config/settings.py
@@ -142,6 +142,7 @@ LANGUAGE_COOKIE_HTTPONLY = False
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # ======================
 # TEMPLATES

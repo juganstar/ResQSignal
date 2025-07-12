766c87fdd995a5b619c2bce13eccf5b4c625105b
Valdemar Santos <valdemarnoivo@hotmail.com>
2025-07-12 23:08:22 +0100

    Fix: add missing get_current_site() to CustomAccountAdapter

--- backend/users/adapters.py
+++ backend/users/adapters.py
@@ -1,16 +1,12 @@
 from allauth.account.adapter import DefaultAccountAdapter
-from allauth.account.utils import user_email
+from django.contrib.sites.shortcuts import get_current_site
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
     def send_confirmation_mail(self, request, emailconfirmation, signup):
-        # ✅ Método manual para substituir o que estava a falhar
-        ctx = {
-            "user": emailconfirmation.email_address.user,
-            "current_site": self.get_current_site(request),
-            "key": emailconfirmation.key,
-            "activate_url": self.get_email_confirmation_url(request, emailconfirmation),
-        }
-        email = user_email(emailconfirmation.email_address.user)
-        self.send_mail("account/email/email_confirmation_message", email, ctx)
+        ctx = self.get_email_confirmation_context(request, emailconfirmation)
+        ctx["current_site"] = self.get_current_site(request)
+        self.send_mail("account/email/email_confirmation_message", emailconfirmation.email_address.email, ctx)
 
+    def get_current_site(self, request):
+        return get_current_site(request)

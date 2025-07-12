14ca44f7b4dc9b78bd01ee1bc0b23a324fb30a50
Valdemar Santos <valdemarnoivo@hotmail.com>
2025-07-12 23:13:07 +0100

    Fix: full CustomAccountAdapter with email context and current_site

--- backend/users/adapters.py
+++ backend/users/adapters.py
@@ -1,12 +1,29 @@
 from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account.utils import user_email
+from allauth.account.models import EmailConfirmation
 from django.contrib.sites.shortcuts import get_current_site
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
+    def get_email_confirmation_context(self, request, emailconfirmation):
+        """
+        Fixes the error:
+        AttributeError: 'CustomAccountAdapter' object has no attribute 'get_email_confirmation_context'
+        """
+        return {
+            "user": emailconfirmation.email_address.user,
+            "activate_url": emailconfirmation.get_absolute_url(),
+            "current_site": self.get_current_site(request),
+            "key": emailconfirmation.key,
+        }
+
     def send_confirmation_mail(self, request, emailconfirmation, signup):
         ctx = self.get_email_confirmation_context(request, emailconfirmation)
-        ctx["current_site"] = self.get_current_site(request)
-        self.send_mail("account/email/email_confirmation_message", emailconfirmation.email_address.email, ctx)
+        self.send_mail(
+            "account/email/email_confirmation_message",
+            emailconfirmation.email_address.email,
+            ctx,
+        )
 
     def get_current_site(self, request):
         return get_current_site(request)

4c917358b816933cff161c1e27546f68ee49809a
Valdemar Santos <valdemarnoivo@hotmail.com>
2025-07-27 05:43:41 +0100

    Remove invalid allauth middleware

--- config/settings.py
+++ config/settings.py
@@ -66,8 +66,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
-
 ]
 
 ROOT_URLCONF = "config.urls"

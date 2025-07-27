d3edd03b686d6d61a585be26a53f109f6d9cc855
Valdemar Santos <valdemarnoivo@hotmail.com>
2025-07-27 02:51:49 +0100

    Update database config for Railway deployment

--- config/settings.py
+++ config/settings.py
@@ -76,18 +76,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 # ======================
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
-        'CONN_MAX_AGE': 600,
-        'OPTIONS': {
-            'connect_timeout': 10,
-        },
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 
 # ======================
